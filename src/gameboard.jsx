b87b4e5f905ccc03d9e984ff2a39c37ad354f416
late-bloomer82 <nateriver72@outlook.com>
2025-01-09 22:12:41 -0500

    update score states  and wasClicked status on click

--- src/gameboard.jsx
+++ src/gameboard.jsx
@@ -1,6 +1,44 @@
 import Card from "./card";
 
-function Gameboard({ pokemonData }) {
+function Gameboard({
+  pokemonData,
+  setPokemonData,
+  score,
+  bestScore,
+  setScore,
+  setBestScore,
+}) {
+  const updateClickedProperty = (clickedPokemon) => {
+    setPokemonData((prevData) =>
+      prevData.map(
+        (pokemon) =>
+          pokemon.id === clickedPokemon.id // Replace targetId with the actual ID of the pokemon you're targeting
+            ? { ...pokemon, wasClicked: true } // Update the wasClicked property
+            : pokemon // Leave the rest of the pokemons unchanged
+      )
+    );
+  };
+  const handleClick = (pokemon) => {
+    if (!pokemon.wasClicked) {
+      setScore((previousScore) => {
+        const newScore = previousScore + 1;
+        setBestScore((previousBestScore) =>
+          newScore > previousBestScore ? newScore : previousBestScore
+        );
+        updateClickedProperty(pokemon);
+
+        return newScore;
+      });
+    } else {
+      setScore((previousScore) => {
+        setBestScore((previousBestScore) =>
+          previousScore > previousBestScore ? previousScore : previousBestScore
+        );
+        return 0; // Reset the score
+      });
+    }
+  };
+
   return (
     <section id="gameboard">
       {pokemonData.map((pokemon) => (
@@ -8,6 +46,9 @@ function Gameboard({ pokemonData }) {
           key={pokemon.id}
           name={pokemon.name}
           image={pokemon.images.sprite}
+          onClick={() => {
+            handleClick(pokemon);
+          }}
         ></Card>
       ))}
     </section>
